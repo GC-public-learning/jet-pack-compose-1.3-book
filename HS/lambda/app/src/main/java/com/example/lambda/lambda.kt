755f4bb5e0c0159e04d19ead08dcf3a003b813b1
Geoffrey Carpentier <geoffrey.m.carpentier@gmail.com>
2024-03-15 17:56:37 +0100

    update

--- HS/lambda/app/src/main/java/com/example/lambda/lambda.kt
+++ HS/lambda/app/src/main/java/com/example/lambda/lambda.kt
@@ -33,6 +33,11 @@ fun main() {
     println(fun1(::convertIntToString, 10)) // :: a named function as param
     println(fun1(::convertIntToStringAndHelloMessage, 20)) // :: a named function as param
     println(fun1( { num: Int -> num.toString() }, 30)) // a lambda as param
+
+    // works also without specifying the type (already in high order fun)
+    println(fun1( { num -> num.toString() }, 40))
+
+    // of course if the lambda doesn't belong an high order fun, the type needs to be there
     val const1 = { num: Int -> num.toString() }
     println(fun1(const1, 10)) // a const whose a lambada is assigned to as param
 
@@ -44,20 +49,38 @@ fun main() {
         num = var1 * 10 // body
         num// return (no assigniation, just a value )
     }
-    fun2(const2, 100, 200)
+    println(fun2(const2, 100, 200))
 
     val const3 = { num: Int, num2 : Int -> num + num2 }
-    fun2(const3, 10, 20)
+    println(fun2(const3, 10, 20))
+
     // --------------------------------------------------
 
+    // fun affectation
     val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+    // or
+    val sum2 = { x: Int, y: Int -> x + y } // explicit declaration omitted
+    // or
+
+    fun sumOfxy(x:Int, y:Int): Int = x + y
+    val sum3 = ::sumOfxy
+
+    //or
+    fun sumOfxy2(x:Int, y:Int): Int {
+        return x + y
+    }
+    val sum4 = ::sumOfxy
+
+
+    println(sum(5,3))
 
 }
-
+// -------------------------------------------------------------------------------------------
+// high order fun (fun that takes another fun as param)
 // lambdaName = block
 // inputType = ()
 // returnType = Unit (no return)
-fun lineLogger(block : () -> Unit, a : Int) { // high order fun
+fun lineLogger(block : () -> Unit, a : Int) {
     repeat(5, { println("-----") })
     block()
     repeat(5, { println("-----") })
@@ -69,6 +92,7 @@ fun convertIntToString (num : Int) : String {
 fun convertIntToStringAndHelloMessage (num : Int) : String {
     return "Hello ! $num"
 }
+
 // high order fun
 fun fun1 (fun2 : (Int) -> String, num : Int) : String {
     return fun2(num)
